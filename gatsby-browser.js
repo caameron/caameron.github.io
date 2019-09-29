056a95b505351858fa0b563fd2febbeb1155a64a
caameron <30531788+caameron@users.noreply.github.com>
2019-09-28 21:20:53 -0700

    Updates

--- gatsby-browser.js
+++
@@ -1,3 +0,0 @@
-import "./src/layouts/tachyons/css/tachyons.css"
-import "./node_modules/tachyons-animate/css/tachyons-animate.css"
-import "./src/layouts/tachyons/css/portfolio.css"
--- gatsby-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: 'Gatsby Tachyons',
-  },
-  plugins: ['gatsby-plugin-react-helmet'],
-  plugins: [`gatsby-plugin-postcss`],
-}
